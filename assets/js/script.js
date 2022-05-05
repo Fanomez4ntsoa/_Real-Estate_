9fb942b95bd440f82c065c70d9d63686d4cf57a6
Fanomez4ntsoa <voary.randrianarison@esti.mg>
2022-05-05 22:19:57 +0300

    Configuration of the home section, logos, popular with Css

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,18 @@ function scrollHeader(){
     // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
     if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
-window.addEventListener('scroll', scrollHeader)
+window.addEventListener('scroll', scrollHeader);
+
+/* Swiper popular */
+var swiperPopular = new Swiper(".popular__container", {
+	spaceBetween: 32,
+	grabCursor: true,
+	centeredSlides: true,
+	slidesPerView: 'auto',
+	loop: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
